61ab06765c7c2ef6925e0f60e0a8cd36f68149db
Prog. Apps Android L y M de 15:30 a 20:30 <android-ed1@aula.local>
2016-04-20 16:47:24 +0200

    Browser 20/04/2016

---
+++ app/src/main/java/info/wwwood/browser/MainActivity.java
@@ -0,0 +1,31 @@
+package info.wwwood.browser;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getIntent().getData()!=null) {
+            String urlString=getIntent().getData().toString();
+
+            WebView main_wvNavegador = (WebView) findViewById(R.id.main_wvNavegador);
+            main_wvNavegador.setWebViewClient(new WebViewClient()); // perquè les redireccions les gestioni el propi browser
+            main_wvNavegador.loadUrl(urlString);
+
+        }
+
+       // main_wbNavegador.loadUrl(data);
+
+
+    }
+}
